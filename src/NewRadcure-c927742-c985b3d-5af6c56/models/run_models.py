08de7fe26556ec12b02f9ad09bea413cc503990b
Mogtaba-Alim <alim.mogtaba@gmail.com>
2024-05-21 14:01:54 -0400

    Fixed data paths

--- src/NewRadcure-c927742-c985b3d-5af6c56/models/run_models.py
+++ src/NewRadcure-c927742-c985b3d-5af6c56/models/run_models.py
@@ -29,8 +29,8 @@ def split_zeros(group, training_ratio, test_ratio):
 
 if __name__ == '__main__':
 
-    df_radiomic = pd.read_csv('/Users/maximus/Desktop/FALL2023/BCB430/code/headNeckModels/ClinicalData/RADCURE_complete_radiomics_features.csv')
-    df_outcomes = pd.read_csv('/Users/maximus/Desktop/FALL2023/BCB430/code/headNeckModels/ClinicalData/RADCURE-DA-CLINICAL-2.csv')
+    df_radiomic = pd.read_csv('/Users/maximus/Desktop/FALL2023/BCB430/code/headNeckModels/ClinicalData/RADCURE/RADCURE_complete_radiomics_features.csv')
+    df_outcomes = pd.read_csv('/Users/maximus/Desktop/FALL2023/BCB430/code/headNeckModels/ClinicalData/RADCURE/RADCURE-DA-CLINICAL-2.csv')
     output_path = '/Users/maximus/Desktop/FALL2023/BCB430/code/headNeckModels/CancerPredictiveModelsHeadNeck/Output/NewRadcure'
 
     if not os.path.exists('/Users/maximus/Desktop/FALL2023/BCB430/code/headNeckModels/CancerPredictiveModelsHeadNeck/Output'):
